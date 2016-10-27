67d0b4068b093bad91621711b492ed7730896eef
Nico Malig <nicomalig@gmail.com>
2016-10-27 10:14:06 -0700

    Semi-complete e2

--- exercise-2/js/main.js
+++ exercise-2/js/main.js
@@ -2,10 +2,17 @@
 $(function() {
 
 	// Setup: Initialize Firebase using the configuration of your project
-
+	var config = {
+		apiKey: "AIzaSyAJVBLPgHFpeNYy20K96TlkJ7AHuNg5tA8",
+		authDomain: "m12-e2.firebaseapp.com",
+		databaseURL: "https://m12-e2.firebaseio.com",
+		storageBucket: "m12-e2.appspot.com",
+		messagingSenderId: "1087625218462"
+	};
+	firebase.initializeApp(config);
 
 	// Set a reference to a "photos" point in your database
-
+	
 
 	// Create a variable to store the firebase storage object
 
@@ -36,5 +43,3 @@ $(function() {
 			// Get the download URL from the reference, then...
 
 				// Push the URL as a new child into your data structure
-
-		
